93a5f8cf60e0bcfb3ee73602e0390156b9a6ded9
Obaidul1545 <mdobaidulislam128@gmail.com>
2025-11-09 02:09:28 +0600

    server create express and mongodb connected

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleqares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is Running');
+});
+
+const uri =
+  'mongodb+srv://study-mate:rvvb2WP9DgroU1pV@simple-crud-project.arynq4d.mongodb.net/?appName=simple-crud-project';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`StudyMate app listening on port ${port}`);
+});
